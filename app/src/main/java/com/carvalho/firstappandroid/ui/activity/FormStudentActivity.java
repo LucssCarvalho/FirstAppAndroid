c73130a955bd4661cccca4122cab7f735bb50f32
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-07-15 14:53:09 -0300

    add edit students

--- app/src/main/java/com/carvalho/firstappandroid/ui/activity/FormStudentActivity.java
+++ app/src/main/java/com/carvalho/firstappandroid/ui/activity/FormStudentActivity.java
@@ -1,34 +1,53 @@
 package com.carvalho.firstappandroid.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.carvalho.firstappandroid.R;
 import com.carvalho.firstappandroid.dao.StudentDAO;
 import com.carvalho.firstappandroid.model.Student;
+import static com.carvalho.firstappandroid.ui.activity.ConstantActivities.KEY_STUDENT;
 
 public class FormStudentActivity extends AppCompatActivity {
 
-    public static final String TITLE_APPBAR = "Novo aluno";
+    private static final String TITLE_APPBAR_NEW_STUDENT = "Novo aluno";
+    private static final String TITLE_APPBAR_EDIT_STUDENT = "Edita aluno";
     private EditText inputName;
     private EditText inputPhone;
     private EditText inputEmail;
     final StudentDAO dao = new StudentDAO();
+    private Student student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_student);
-
-        setTitle(TITLE_APPBAR);
         initInputs();
         findViewById(R.id.activity_form_student_button_save);
         configureButtonSave();
+        getStudent();
+    }
+
+    private void getStudent() {
+        final Intent data = getIntent();
+        if (data.hasExtra(KEY_STUDENT)) {
+            student = (Student) data.getSerializableExtra(KEY_STUDENT);
+            fillInput();
+            setTitle(TITLE_APPBAR_EDIT_STUDENT);
+        } else {
+            setTitle(TITLE_APPBAR_NEW_STUDENT);
+            student = new Student();
+        }
+
+    }
+
+    private void fillInput() {
+        inputName.setText(student.getName());
+        inputPhone.setText(student.getPhone());
+        inputEmail.setText(student.getEmail());
     }
 
     private void configureButtonSave() {
@@ -36,10 +55,19 @@ public class FormStudentActivity extends AppCompatActivity {
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Student student = createStudent();
-                save(student);
+                finishForm();
             }
         });
+    }
+
+    private void finishForm() {
+        fillStudent();
+        if (student.hasValidId()) {
+            dao.edit(student);
+        } else {
+            dao.save(student);
+        }
+        finish();
     }
 
     private void initInputs() {
@@ -48,18 +76,12 @@ public class FormStudentActivity extends AppCompatActivity {
         inputEmail = findViewById(R.id.activity_form_student_mail);
     }
 
-    private void save(Student student) {
-        dao.save(student);
-
-        finish();
-    }
-
-    @NonNull
-    private Student createStudent() {
+    private void fillStudent() {
         String name = inputName.getText().toString();
         String phone = inputPhone.getText().toString();
         String email = inputEmail.getText().toString();
-        Student student = new Student(name, phone, email);
-        return student;
+        student.setName(name);
+        student.setPhone(phone);
+        student.setEmail(email);
     }
 }
--- app/src/main/java/com/carvalho/firstappandroid/ui/activity/ListStudentsActivity.java
+++ app/src/main/java/com/carvalho/firstappandroid/ui/activity/ListStudentsActivity.java
@@ -3,14 +3,19 @@ package com.carvalho.firstappandroid.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.carvalho.firstappandroid.R;
 import com.carvalho.firstappandroid.dao.StudentDAO;
+import com.carvalho.firstappandroid.model.Student;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+import static com.carvalho.firstappandroid.ui.activity.ConstantActivities.KEY_STUDENT;
 
 public class ListStudentsActivity extends AppCompatActivity {
 
@@ -21,9 +26,7 @@ public class ListStudentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_students);
-
         setTitle(TITLE_APPBAR);
-
         configureFabNewStudent();
     }
 
@@ -32,27 +35,47 @@ public class ListStudentsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openFormStudentActivity();
+                openFormForCreateStudent();
             }
         });
     }
 
-    private void openFormStudentActivity() {
+    private void openFormForCreateStudent() {
         startActivity(new Intent(this,
                 FormStudentActivity.class));
     }
 
     @Override
     protected void onResume() {
-
         super.onResume();
         configList();
     }
 
     private void configList() {
         ListView listStudents = findViewById(R.id.activity_main_list_students);
+        final List<Student> students = dao.allStudents();
+        configAdapter(listStudents);
+        configListernForAnItem(listStudents);
+    }
+
+    private void configListernForAnItem(ListView listStudents) {
+        listStudents.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Student studentSelected = (Student) adapterView.getItemAtPosition(position);
+                openFormForEditStudent(studentSelected);
+            }
+        });
+    }
+
+    private void openFormForEditStudent(Student studentSelected) {
+        Intent navigateForm = new Intent(ListStudentsActivity.this, FormStudentActivity.class);
+        navigateForm.putExtra(KEY_STUDENT, studentSelected);
+        startActivity(navigateForm);
+    }
+
+    private void configAdapter(ListView listStudents) {
         listStudents.setAdapter(new ArrayAdapter<>(
-                this, android.R.layout.simple_list_item_1,dao.allStudents()
-        ));
+                this, android.R.layout.simple_list_item_1, dao.allStudents()));
     }
 }
