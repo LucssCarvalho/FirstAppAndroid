601c50cc0f0a3540977c17322a20b682cfa4dc9f
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-07-15 00:50:14 -0300

    refactoring ListStudentsActivity

--- app/src/main/java/com/carvalho/firstappandroid/ui/activity/ListStudentsActivity.java
+++ app/src/main/java/com/carvalho/firstappandroid/ui/activity/ListStudentsActivity.java
@@ -1,40 +1,55 @@
 package com.carvalho.firstappandroid.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.carvalho.firstappandroid.R;
 import com.carvalho.firstappandroid.dao.StudentDAO;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ListStudentsActivity extends AppCompatActivity {
 
+    public static final String TITLE_APPBAR = "Lista de alunos";
+    private StudentDAO dao = new StudentDAO();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_students);
 
-        setTitle("Lista de alunos");
+        setTitle(TITLE_APPBAR);
 
+        configureFabNewStudent();
+    }
+
+    private void configureFabNewStudent() {
         FloatingActionButton fab = findViewById(R.id.activity_main_fab_new_student);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ListStudentsActivity.this,
-                        FormStudentActivity.class));
+                openFormStudentActivity();
             }
         });
     }
 
+    private void openFormStudentActivity() {
+        startActivity(new Intent(this,
+                FormStudentActivity.class));
+    }
+
     @Override
     protected void onResume() {
-        StudentDAO dao = new StudentDAO();
 
         super.onResume();
+        configList();
+    }
+
+    private void configList() {
         ListView listStudents = findViewById(R.id.activity_main_list_students);
         listStudents.setAdapter(new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1,dao.allStudents()
