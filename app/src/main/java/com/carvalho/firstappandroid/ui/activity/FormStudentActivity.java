8dc8ff55415b1942a84b92ec1eff4b9bb4b9fb49
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-07-14 22:05:00 -0300

    update form student activity

--- app/src/main/java/com/carvalho/firstappandroid/ui/activity/FormStudentActivity.java
+++ app/src/main/java/com/carvalho/firstappandroid/ui/activity/FormStudentActivity.java
@@ -1,14 +1,15 @@
 package com.carvalho.firstappandroid.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.carvalho.firstappandroid.R;
+import com.carvalho.firstappandroid.dao.StudentDao;
 import com.carvalho.firstappandroid.model.Student;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FormStudentActivity extends AppCompatActivity {
 
@@ -16,6 +17,9 @@ public class FormStudentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_student);
+
+        StudentDao dao = new StudentDao();
+
         final EditText inputName = findViewById(R.id.activity_form_student_name);
         final EditText inputPhone = findViewById(R.id.activity_form_student_phone);
         final EditText inputEmail = findViewById(R.id.activity_form_student_mail);
@@ -23,7 +27,6 @@ public class FormStudentActivity extends AppCompatActivity {
         findViewById(R.id.activity_form_student_button_save);
 
         Button buttonSave = findViewById(R.id.activity_form_student_button_save);
-
         buttonSave.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -32,11 +35,9 @@ public class FormStudentActivity extends AppCompatActivity {
                 String email = inputEmail.getText().toString();
 
                 Student student = new Student(name, phone,email);
-                Toast.makeText(FormStudentActivity.this,
-                        student.getName() + "-"
-                        + student.getPhone() + "-"
-                        + student.getEmail(), Toast.LENGTH_LONG).show();
-                
+                dao.save(student);
+
+                finish();
             }
         });
 
