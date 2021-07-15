3d0dc3e151c0cded26443aaf2c8444de97abe116
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2021-07-15 14:52:36 -0300

    update student dao

--- app/src/main/java/com/carvalho/firstappandroid/dao/StudentDAO.java
+++ app/src/main/java/com/carvalho/firstappandroid/dao/StudentDAO.java
@@ -1,5 +1,7 @@
 package com.carvalho.firstappandroid.dao;
 
+import androidx.annotation.NonNull;
+
 import com.carvalho.firstappandroid.model.Student;
 
 import java.util.ArrayList;
@@ -8,8 +10,39 @@ import java.util.List;
 public class StudentDAO {
 
     private final static List<Student> students = new ArrayList<>();
+    private static int countIds = 1;
 
-    public void save(Student student){ students.add(student); }
+    public void save(Student student) {
+        student.setId(countIds);
+        students.add(student);
+        updateIds();
+    }
 
-    public List<Student> allStudents() { return new ArrayList<>(students); }
+    private void updateIds() {
+        countIds++;
+    }
+
+    public void edit(Student student) {
+        Student studentFound = searchStudent(student);
+        if (studentFound != null) {
+            int positionStudent = students.indexOf(studentFound);
+            students.set(positionStudent, student);
+        }
+    }
+
+    @NonNull
+    private Student searchStudent(Student student) {
+        Student studentFound = null;
+        for (Student i :
+                students) {
+            if (i.getId() == student.getId()) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public List<Student> allStudents() {
+        return new ArrayList<>(students);
+    }
 }
